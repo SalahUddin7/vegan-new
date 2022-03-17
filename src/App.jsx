0d737eaf42e6261d372722843f66cdc36fd1a1f7
Salah Uddin <ziko.salahuddin@gmail.com>
2022-03-17 19:12:09 +0100

    React Router -Dom Installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+//NPM Packages
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+//Project files
+import Category from './pages/Catagory';
+import Contacts from './pages/Contacts';
+import Home from './pages/Home';
+import Product from './pages/Product';
+
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello World-3</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/:category" element={<Category />}></Route>
+          <Route path="/:category" element={<Contacts />}></Route>
+
+          <Route path="/:product" element={<Product />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/pages/Contact.jsx
+++
@@ -1,3 +0,0 @@
-export default function Contact() {
-  return <div id="contact">contact</div>;
-}
