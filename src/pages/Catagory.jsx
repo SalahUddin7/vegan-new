bdcd4cabf07f5f50b0a6e52fa2d26df2c31e87ad
Salah Uddin <ziko.salahuddin@gmail.com>
2022-03-17 16:03:11 +0100

    create the minimum code necessary to start writing the routes

---
+++ src/pages/Catagory.jsx
@@ -0,0 +1,7 @@
+export default function Category() {
+  return (
+    <div id="categry">
+      <h1>Category-1</h1>
+    </div>
+  );
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div id="home">
+      <h1>Home-1</h1>
+    </div>
+  );
+}
