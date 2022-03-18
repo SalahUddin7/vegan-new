dff93c0b3137dca01d75f86730f6d0e70fdbacaa
Salah Uddin <ziko.salahuddin@gmail.com>
2022-03-18 13:26:55 +0100

    Master branch is working

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div id="home">
-      <h1>This is Home Content</h1>
+      <h1>This is Home Content- Master branch is working </h1>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
         asperiores quam distinctio atque rerum, nisi nesciunt in iste temporibus
